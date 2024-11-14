99bd4ba81a8e004e7c294605ee9d8170f9cccf22
msemianko <maks.semenko@gmail.com>
2024-11-14 05:44:42 -0800

    Added DatabaseInitializerConfig

---
+++ src/main/java/com/maxsoft/datingbackend/config/DatabaseInitializerConfig.java
@@ -0,0 +1,36 @@
+package com.maxsoft.datingbackend.config;
+
+import com.maxsoft.datingbackend.user.gender.GenderEnum;
+import com.maxsoft.datingbackend.user.gender.GenderModel;
+import com.maxsoft.datingbackend.user.gender.GenderRepository;
+import com.maxsoft.datingbackend.user.role.RoleEnum;
+import com.maxsoft.datingbackend.user.role.RoleModel;
+import com.maxsoft.datingbackend.user.role.RoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class DatabaseInitializerConfig {
+
+    private RoleRepository roleRepository;
+    private GenderRepository genderRepository;
+
+    @Bean
+    public CommandLineRunner initDatabase() {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                roleRepository.save(RoleModel.builder().name(RoleEnum.ROLE_ADMIN).build());
+                roleRepository.save(RoleModel.builder().name(RoleEnum.ROLE_USER).build());
+                roleRepository.save(RoleModel.builder().name(RoleEnum.ROLE_SUPER_USER).build());
+            }
+            if (genderRepository.count() == 0) {
+                genderRepository.save(GenderModel.builder().name(GenderEnum.MALE).build());
+                genderRepository.save(GenderModel.builder().name(GenderEnum.FEMALE).build());
+            }
+        };
+    }
+}
+
